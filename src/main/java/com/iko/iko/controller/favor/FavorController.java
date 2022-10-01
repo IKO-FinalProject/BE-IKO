e3e3349f14c0a500db91f1b48d3a1cedf780e7bb
TaeHye0n <thk001229@gmail.com>
2022-10-01 11:48:51 +0900

    feat:modify myreplyList Api

---
+++ src/main/java/com/iko/iko/controller/favor/FavorController.java
@@ -0,0 +1,10 @@
+package com.iko.iko.controller.favor;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/favor")
+@RestController
+public class FavorController {
+}
---
+++ src/main/java/com/iko/iko/service/member/AddFavorService.java
@@ -0,0 +1,29 @@
+package com.iko.iko.service.member;
+
+import com.iko.iko.common.exception.BaseException;
+import com.iko.iko.common.response.ErrorCode;
+import com.iko.iko.domain.entity.Member;
+import com.iko.iko.domain.repository.member.MemberRepository;
+import com.iko.iko.security.jwt.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class AddFavorService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long myReplyList(Integer productId) {
+        Member member = validateLoginStatus();
+        return memberRepository.addFavor(member.getMemberId(), productId);
+    }
+
+    public Member validateLoginStatus() {
+        return memberRepository.findByEmail(SecurityUtil.getLoginUserEmail())
+                .orElseThrow(() -> new BaseException(ErrorCode.NEED_LOGIN));
+    }
+}
