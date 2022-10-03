911bcb5b995b94e898165917a1cd53faac5b8657
sungbin <psb4759@nate.com>
2022-10-04 04:34:35 +0900

    feat : update productMainApi-add isFavorite

--- src/main/java/com/iko/iko/service/product/GetAllProductService.java
+++ src/main/java/com/iko/iko/service/product/GetAllProductService.java
@@ -1,9 +1,14 @@
 package com.iko.iko.service.product;
 
+import com.iko.iko.common.exception.BaseException;
+import com.iko.iko.common.response.ErrorCode;
 import com.iko.iko.controller.ProductDetails.dto.ProductDetailsResponse;
 import com.iko.iko.controller.product.dto.ProductResponse;
+import com.iko.iko.domain.entity.Member;
+import com.iko.iko.domain.repository.member.MemberRepository;
 import com.iko.iko.domain.repository.product.ProductRepository;
 import com.iko.iko.domain.repository.productDetails.ProductDetailsRepository;
+import com.iko.iko.security.jwt.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,12 +21,16 @@ import java.util.List;
 public class GetAllProductService {
     private final ProductDetailsRepository productDetailsRepository;
     private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
     public List<ProductDetailsResponse.MainProductForResponse> GetMainProduct(Pageable pageable) {
+        Member member = validateLoginStatus();
+
         List<ProductDetailsResponse.MainProductForResponse> result = new ArrayList<>();
 
         List<ProductResponse.GetAllProductDistinct> mainProduct = productRepository.getAllProduct();
 
         for(ProductResponse.GetAllProductDistinct tmp : mainProduct){
+            Long isFavorite = productRepository.getMemberIsFavorite(member.getMemberId(), tmp.getProductId());
             List<ProductDetailsResponse.GetGraphicDiameter> graphicList = productDetailsRepository.getGraphic(tmp.getProductId());
             List<Float> gList = new ArrayList<>();
             List<ProductDetailsResponse.GetColorCodeAndImageUrl> iList = new ArrayList<>();
@@ -40,6 +49,7 @@ public class GetAllProductService {
             }
             ProductDetailsResponse.MainProductForResponse checkData
                     =new ProductDetailsResponse.MainProductForResponse(
+                            isFavorite,
                     tmp.getProductId(),
                     tmp.getSeries(),
                     gList,
@@ -50,5 +60,10 @@ public class GetAllProductService {
             result.add(checkData);
         }
         return result;
+    }
+
+    public Member validateLoginStatus() {
+        return memberRepository.findByEmail(SecurityUtil.getLoginUserEmail())
+                .orElseThrow(() -> new BaseException(ErrorCode.NEED_LOGIN));
     }
 }
