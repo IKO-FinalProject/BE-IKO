27a82654e9528e0c717d67f5194a6b7f50f26f14
TaeHye0n <thk001229@gmail.com>
2022-10-06 09:21:13 +0900

    feat: addreply api

--- src/main/java/com/iko/iko/controller/reply/ReplyController.java
+++ src/main/java/com/iko/iko/controller/reply/ReplyController.java
@@ -2,22 +2,27 @@ package com.iko.iko.controller.reply;
 
 import com.iko.iko.common.response.Response;
 import com.iko.iko.controller.reply.dto.request.ReplyRequestDto.AddReplyRequest;
+import com.iko.iko.service.reply.facade.ReplyFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("reply")
 public class ReplyController {
 
-//    @PostMapping("/add")
-//    public ResponseEntity<Response<String>> addReply(
-//            @RequestBody @Valid AddReplyRequest addReplyRequest)
-//    {
-//
-//    }
+    private final ReplyFacade replyFacade;
+    @PostMapping("/add")
+    public ResponseEntity<Response<String>> addReply(
+            @RequestBody @Valid AddReplyRequest addReplyRequest
+    ) {
+        return ResponseEntity.ok(
+                Response.of(replyFacade.addReply(addReplyRequest),
+                        "리뷰 작성 완료")
+        );
+    }
+
 }
