38f14ae239da387a849f23012d3954e8b053b6ef
TaeHye0n <thk001229@gmail.com>
2022-10-07 19:28:14 +0900

    feat:addcoupon for member api

--- src/main/java/com/iko/iko/controller/coupon/CouponController.java
+++ src/main/java/com/iko/iko/controller/coupon/CouponController.java
@@ -1,12 +1,30 @@
 package com.iko.iko.controller.coupon;
 
+import com.iko.iko.common.response.Response;
+import com.iko.iko.service.coupon.facade.CouponFacade;
+import com.iko.iko.controller.coupon.dto.request.CouponRequestDto.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/coupon")
 public class CouponController {
 
+    private final CouponFacade couponFacade;
+
+    @PostMapping("/add")
+    public ResponseEntity<Response<String>> addCoupon(
+            @RequestBody @Valid AddCouponRequest addCouponRequest) {
+        return ResponseEntity.ok(
+                Response.of(couponFacade.addCoupon(addCouponRequest),
+                        "쿠폰 발급 완료")
+        );
+    }
 }
--- src/main/java/com/iko/iko/service/coupon/facade/CouponFacade.java
+++ src/main/java/com/iko/iko/service/coupon/facade/CouponFacade.java
@@ -1,9 +1,18 @@
 package com.iko.iko.service.coupon.facade;
 
+import com.iko.iko.controller.coupon.dto.request.CouponRequestDto.*;
+import com.iko.iko.service.coupon.AddCouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CouponFacade {
+
+    private final AddCouponService addCouponService;
+    @Transactional
+    public String addCoupon(AddCouponRequest addCouponRequest){
+        return addCouponService.addCoupon(addCouponRequest);
+    }
 }
